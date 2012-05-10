8b592b73ef97fe32cfd925db6697b170e74ddace
Mary Ann Valentine <maryannvalentine@mary-ann-valentines-macbook-pro-7.local>
2012-05-09 22:45:52 -0700

    making the NIRD-tasks repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NIRDTasks::Application.config.secret_token = '77c921a4491c1c70f635c6133f3b550e6f089122c46a187c88acaf94c544cfcd338878f9fc95cbf0c56cee8cd9898b5d0c256d2e94af98ca58b2593bdddf88f7'
