5ff0a2ea951934b9cbd2321a5c845199f10c6198
Adam Hendricksen <adam.hendricksen@gmail.com>
2012-05-14 21:34:26 -0700

    added heading columns in the index to make it look better and to better designate their functions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120511231517) do
+ActiveRecord::Schema.define(:version => 20120513201837) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120511231517) do
     t.integer  "importance"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "completed"
   end
 
 end
