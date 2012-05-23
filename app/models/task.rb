f638ee7cc718a7a607eae896f1c08cfe528a3544
Mary Ann Valentine <maryann.valentine@gmail.com>
2012-05-22 21:12:33 -0700

    Wanted to limit the range of responses for importance so nobody could put 1 billion

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :due, :importance, :name
+  validates_inclusion_of :importance, :in => 1..9, :message => "must be a number between 1 and 9."
 end
